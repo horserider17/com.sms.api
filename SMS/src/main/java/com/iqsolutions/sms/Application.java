0bf4e9bd0911e8260e230171f49f4f6a53fad6dc
Rakesh Sharma <eee.rakeshsharma@gmailcom>
2019-03-13 22:40:55 +0530

    Login service impl

--- SMS/src/main/java/com/iqsolutions/sms/Application.java
+++ SMS/src/main/java/com/iqsolutions/sms/Application.java
@@ -2,11 +2,12 @@ package com.iqsolutions.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
